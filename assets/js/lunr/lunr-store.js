454ec82be252169e68c47016ae7ce8944153b428
mnyrop <mnyrop@users.noreply.github.com>
2025-01-24 19:02:27 +0000

    deploy: 2716269ee22a5b158442b9df61f28f943452d44d

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,397 @@
-var store = []
+var store = [{
+        "title": "CrossAsia Integrated Text repository",
+        "excerpt":" ","categories": ["Cross-collection search and discovery"],
+        "tags": [],
+        "url": "/resources/1/",
+        "teaser": null
+      },{
+        "title": "Chinese Historiography Bibliography",
+        "excerpt":" ","categories": ["Recommended Subject Guides, Bibliographies, and Translations"],
+        "tags": [],
+        "url": "/resources/10/",
+        "teaser": null
+      },{
+        "title": "Xianggang wen xue zi liao ku",
+        "excerpt":" ","categories": ["Recommended Subject Guides, Bibliographies, and Translations"],
+        "tags": [],
+        "url": "/resources/11/",
+        "teaser": null
+      },{
+        "title": "Chinese Christian Texts Database",
+        "excerpt":" ","categories": ["Recommended Subject Guides, Bibliographies, and Translations"],
+        "tags": [],
+        "url": "/resources/12/",
+        "teaser": null
+      },{
+        "title": "Ming History English Translation Project",
+        "excerpt":" ","categories": ["Recommended Subject Guides, Bibliographies, and Translations"],
+        "tags": [],
+        "url": "/resources/13/",
+        "teaser": null
+      },{
+        "title": "Ming Qing Women's Writings",
+        "excerpt":" ","categories": ["Recommended Subject Guides, Bibliographies, and Translations"],
+        "tags": [],
+        "url": "/resources/14/",
+        "teaser": null
+      },{
+        "title": "Modern Chinese Literature and Culture (MCLC) Resource Center",
+        "excerpt":" ","categories": ["Recommended Subject Guides, Bibliographies, and Translations"],
+        "tags": [],
+        "url": "/resources/15/",
+        "teaser": null
+      },{
+        "title": "PRC History",
+        "excerpt":" ","categories": ["Recommended Subject Guides, Bibliographies, and Translations"],
+        "tags": [],
+        "url": "/resources/16/",
+        "teaser": null
+      },{
+        "title": "Zhongguo gaige kaifang shujuku",
+        "excerpt":" ","categories": ["Recommended Subject Guides, Bibliographies, and Translations"],
+        "tags": [],
+        "url": "/resources/17/",
+        "teaser": null
+      },{
+        "title": "Ivy Plus Libraries Digital Projects on East Asia",
+        "excerpt":" ","categories": ["Recommended Subject Guides, Bibliographies, and Translations"],
+        "tags": [],
+        "url": "/resources/18/",
+        "teaser": null
+      },{
+        "title": "Late Qing and Republican-Era Chinese Newspapers",
+        "excerpt":" ","categories": ["Newspapers (primary sources)"],
+        "tags": [],
+        "url": "/resources/19/",
+        "teaser": null
+      },{
+        "title": "Chinese big sets in IVY-Plus Libraries",
+        "excerpt":" ","categories": ["Cross-collection search and discovery"],
+        "tags": [],
+        "url": "/resources/2/",
+        "teaser": null
+      },{
+        "title": "Bilingual Database and Annotated Bibliography of Cantonese Popular Periodicals of the Early Twentieth Century",
+        "excerpt":" ","categories": ["Newspapers (primary sources)"],
+        "tags": [],
+        "url": "/resources/20/",
+        "teaser": null
+      },{
+        "title": "Old Hong Kong Newspapers",
+        "excerpt":" ","categories": ["Newspapers (primary sources)"],
+        "tags": [],
+        "url": "/resources/21/",
+        "teaser": null
+      },{
+        "title": "East Asian Newspapers and Periodicals 1850-1950",
+        "excerpt":" ","categories": ["Newspapers (primary sources)"],
+        "tags": [],
+        "url": "/resources/22/",
+        "teaser": null
+      },{
+        "title": "Archives of the Institute of Modern History, Academia Sinica",
+        "excerpt":" ","categories": ["Digital Archives, History"],
+        "tags": [],
+        "url": "/resources/23/",
+        "teaser": null
+      },{
+        "title": "Digital Library of Qing Archives",
+        "excerpt":" ","categories": ["Digital Archives, History"],
+        "tags": [],
+        "url": "/resources/24/",
+        "teaser": null
+      },{
+        "title": "Modern History Databases (MHDB)",
+        "excerpt":" ","categories": ["Digital Archives, History"],
+        "tags": [],
+        "url": "/resources/25/",
+        "teaser": null
+      },{
+        "title": "Maoist Legacy",
+        "excerpt":" ","categories": ["Digital Archives, History"],
+        "tags": [],
+        "url": "/resources/26/",
+        "teaser": null
+      },{
+        "title": "China Unofficial Archives",
+        "excerpt":" ","categories": ["Digital Archives, History"],
+        "tags": [],
+        "url": "/resources/27/",
+        "teaser": null
+      },{
+        "title": "Unofficial Poetry Journals from China",
+        "excerpt":" ","categories": ["Digital Archives, History"],
+        "tags": [],
+        "url": "/resources/28/",
+        "teaser": null
+      },{
+        "title": "Early Chinese Periodicals Online (ECPO)",
+        "excerpt":" ","categories": ["Republican Era Material"],
+        "tags": [],
+        "url": "/resources/29/",
+        "teaser": null
+      },{
+        "title": "ReCAP Chinese Journals public list",
+        "excerpt":" ","categories": ["Cross-collection search and discovery"],
+        "tags": [],
+        "url": "/resources/3/",
+        "teaser": null
+      },{
+        "title": "Modernhistory.org.cn Kangzhan wenxian shuju pingtai",
+        "excerpt":" ","categories": ["Republican Era Material"],
+        "tags": [],
+        "url": "/resources/30/",
+        "teaser": null
+      },{
+        "title": "Taiwan eBook",
+        "excerpt":" ","categories": ["Republican Era Material"],
+        "tags": [],
+        "url": "/resources/31/",
+        "teaser": null
+      },{
+        "title": "Global Image Catalogue of Ancient Chinese Book Collections",
+        "excerpt":" ","categories": ["Republican Era Material"],
+        "tags": [],
+        "url": "/resources/32/",
+        "teaser": null
+      },{
+        "title": "Telling stories: Linguistic diversity in Hong Kong",
+        "excerpt":" ","categories": ["Oral History"],
+        "tags": [],
+        "url": "/resources/33/",
+        "teaser": null
+      },{
+        "title": "The Memory Project",
+        "excerpt":" ","categories": ["Oral History"],
+        "tags": [],
+        "url": "/resources/34/",
+        "teaser": null
+      },{
+        "title": "CR/10: China's Cultural Revolution in Memories",
+        "excerpt":" ","categories": ["Oral History"],
+        "tags": [],
+        "url": "/resources/35/",
+        "teaser": null
+      },{
+        "title": "Chinese Religious Text Authority",
+        "excerpt":" ","categories": ["Digital Humanities Tools and Data Sets"],
+        "tags": [],
+        "url": "/resources/36/",
+        "teaser": null
+      },{
+        "title": "Contemporary Chinese Village Gazetteer Data",
+        "excerpt":" ","categories": ["Digital Humanities Tools and Data Sets"],
+        "tags": [],
+        "url": "/resources/37/",
+        "teaser": null
+      },{
+        "title": "Chinese Historical Christian Database",
+        "excerpt":" ","categories": ["Digital Humanities Tools and Data Sets"],
+        "tags": [],
+        "url": "/resources/38/",
+        "teaser": null
+      },{
+        "title": "LoGaRT: Local Gazetteers Research Tools",
+        "excerpt":" ","categories": ["Digital Humanities Tools and Data Sets"],
+        "tags": [],
+        "url": "/resources/39/",
+        "teaser": null
+      },{
+        "title": "East Asian Gateway Service",
+        "excerpt":" ","categories": ["Cross-collection search and discovery"],
+        "tags": [],
+        "url": "/resources/4/",
+        "teaser": null
+      },{
+        "title": "DocuSky",
+        "excerpt":" ","categories": ["Digital Humanities Tools and Data Sets"],
+        "tags": [],
+        "url": "/resources/40/",
+        "teaser": null
+      },{
+        "title": "China.AidData.org",
+        "excerpt":" ","categories": ["Digital Humanities Tools and Data Sets"],
+        "tags": [],
+        "url": "/resources/41/",
+        "teaser": null
+      },{
+        "title": "World Historical Gazetteer",
+        "excerpt":" ","categories": ["Digital Humanities Tools and Data Sets"],
+        "tags": [],
+        "url": "/resources/42/",
+        "teaser": null
+      },{
+        "title": "Chinese Iconography Thesaurus",
+        "excerpt":" ","categories": ["Photographs and Visual Media"],
+        "tags": [],
+        "url": "/resources/43/",
+        "teaser": null
+      },{
+        "title": "Sidney Gamble photographs, 1906-2007",
+        "excerpt":" ","categories": ["Photographs and Visual Media"],
+        "tags": [],
+        "url": "/resources/44/",
+        "teaser": null
+      },{
+        "title": "Hedda Morrison Photographs of China",
+        "excerpt":" ","categories": ["Photographs and Visual Media"],
+        "tags": [],
+        "url": "/resources/45/",
+        "teaser": null
+      },{
+        "title": "East Asia Image Collection",
+        "excerpt":" ","categories": ["Photographs and Visual Media"],
+        "tags": [],
+        "url": "/resources/46/",
+        "teaser": null
+      },{
+        "title": "Historical Photographs of China",
+        "excerpt":" ","categories": ["Photographs and Visual Media"],
+        "tags": [],
+        "url": "/resources/47/",
+        "teaser": null
+      },{
+        "title": "China Biographical Database Project (CBDB)",
+        "excerpt":" ","categories": ["Biographical Databases"],
+        "tags": [],
+        "url": "/resources/48/",
+        "teaser": null
+      },{
+        "title": "Modern China Biographical Database",
+        "excerpt":" ","categories": ["Biographical Databases"],
+        "tags": [],
+        "url": "/resources/49/",
+        "teaser": null
+      },{
+        "title": "National Center for Philosophy & Social Science Documentation",
+        "excerpt":" ","categories": ["Academic articles (secondary sources)"],
+        "tags": [],
+        "url": "/resources/5/",
+        "teaser": null
+      },{
+        "title": "Dictionnaire Biographique de la Chine Occupée / Biographical Dictionary Of Occupied China",
+        "excerpt":" ","categories": ["Biographical Databases"],
+        "tags": [],
+        "url": "/resources/50/",
+        "teaser": null
+      },{
+        "title": "Treasury of Lives",
+        "excerpt":" ","categories": ["Biographical Databases"],
+        "tags": [],
+        "url": "/resources/51/",
+        "teaser": null
+      },{
+        "title": "Xinjiang Documentation Project",
+        "excerpt":" ","categories": ["Contemporary Documents"],
+        "tags": [],
+        "url": "/resources/52/",
+        "teaser": null
+      },{
+        "title": "PRC State Council Archive (English)",
+        "excerpt":" ","categories": ["Contemporary Documents"],
+        "tags": [],
+        "url": "/resources/53/",
+        "teaser": null
+      },{
+        "title": "Xi Jinping Speeches Database",
+        "excerpt":" ","categories": ["Contemporary Documents"],
+        "tags": [],
+        "url": "/resources/54/",
+        "teaser": null
+      },{
+        "title": "Party History Ebook Archive",
+        "excerpt":" ","categories": ["Contemporary Documents"],
+        "tags": [],
+        "url": "/resources/55/",
+        "teaser": null
+      },{
+        "title": "National Library of China e-resources",
+        "excerpt":" ","categories": ["Digital Manuscript and Rare Book Collections"],
+        "tags": [],
+        "url": "/resources/56/",
+        "teaser": null
+      },{
+        "title": "Chinese Rare Book Digital Collection, Library of Congress",
+        "excerpt":" ","categories": ["Digital Manuscript and Rare Book Collections"],
+        "tags": [],
+        "url": "/resources/57/",
+        "teaser": null
+      },{
+        "title": "Harvard Yenching Chinese Rare Books",
+        "excerpt":" ","categories": ["Digital Manuscript and Rare Book Collections"],
+        "tags": [],
+        "url": "/resources/58/",
+        "teaser": null
+      },{
+        "title": "Chinese Text Project",
+        "excerpt":" ","categories": ["Digital Manuscript and Rare Book Collections"],
+        "tags": [],
+        "url": "/resources/59/",
+        "teaser": null
+      },{
+        "title": "Hong Kong Journals Online",
+        "excerpt":" ","categories": ["Academic articles (secondary sources)"],
+        "tags": [],
+        "url": "/resources/6/",
+        "teaser": null
+      },{
+        "title": "Buddhist Digital Archives",
+        "excerpt":" ","categories": ["Digital Manuscript and Rare Book Collections"],
+        "tags": [],
+        "url": "/resources/60/",
+        "teaser": null
+      },{
+        "title": "Zhongguo xieben wenxian shuzi ziyuan ku",
+        "excerpt":" ","categories": ["Digital Manuscript and Rare Book Collections"],
+        "tags": [],
+        "url": "/resources/61/",
+        "teaser": null
+      },{
+        "title": "The China Historical Geographic Information System, CHGIS",
+        "excerpt":" ","categories": ["GIS and Spatial Research"],
+        "tags": [],
+        "url": "/resources/62/",
+        "teaser": null
+      },{
+        "title": "Land Survey Maps of China, 1895-1944",
+        "excerpt":" ","categories": ["GIS and Spatial Research"],
+        "tags": [],
+        "url": "/resources/63/",
+        "teaser": null
+      },{
+        "title": "Modern China Geospatial Database",
+        "excerpt":" ","categories": ["GIS and Spatial Research"],
+        "tags": [],
+        "url": "/resources/64/",
+        "teaser": null
+      },{
+        "title": "The People's Map of Global China",
+        "excerpt":" ","categories": ["GIS and Spatial Research"],
+        "tags": [],
+        "url": "/resources/65/",
+        "teaser": null
+      },{
+        "title": "Mapping Global China",
+        "excerpt":" ","categories": ["GIS and Spatial Research"],
+        "tags": [],
+        "url": "/resources/66/",
+        "teaser": null
+      },{
+        "title": "Chinese Periodicals Index",
+        "excerpt":" ","categories": ["Academic articles (secondary sources)"],
+        "tags": [],
+        "url": "/resources/7/",
+        "teaser": null
+      },{
+        "title": "PubScholar (Chinese)",
+        "excerpt":" ","categories": ["Academic articles (secondary sources)"],
+        "tags": [],
+        "url": "/resources/8/",
+        "teaser": null
+      },{
+        "title": "Biblioteca Virtual de Macau",
+        "excerpt":" ","categories": ["Academic articles (secondary sources)"],
+        "tags": [],
+        "url": "/resources/9/",
+        "teaser": null
+      }]
