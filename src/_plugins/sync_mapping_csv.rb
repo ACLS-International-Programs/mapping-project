535eef9f8ca089485a2be6ab84d10e1b6f5950e6
JM Chris Chang <jmchrischang@gmail.com>
2026-05-13 09:41:48 -0400

    fix build error after plugin

--- src/_plugins/sync_mapping_csv.rb
+++ src/_plugins/sync_mapping_csv.rb
@@ -1,8 +1,11 @@
+require 'fileutils'
+
 Jekyll::Hooks.register :site, :after_init do |site|
   src  = File.join(site.source, '_data', 'mapping-project.csv')
   dest = File.join(site.source, 'assets', 'files', 'mapping-project.csv')
 
   if File.exist?(src)
+    FileUtils.mkdir_p(File.dirname(dest))
     FileUtils.cp(src, dest)
     Jekyll.logger.info "Sync CSV:", "Copied _data/mapping-project.csv → assets/files/mapping-project.csv"
   else
